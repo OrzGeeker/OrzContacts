6a63396d14674a39278acb7b1accee00c26b6e4d
wangzhizhou <wangzhizhou@bytedance.com>
2023-09-28 13:31:13 +0800

    Initial Commit

---
+++ OrzContacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrzContacts
+//
+//  Created by joker on 2023/9/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrzContacts/OrzContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrzContactsApp.swift
+//  OrzContacts
+//
+//  Created by joker on 2023/9/28.
+//
+
+import SwiftUI
+
+@main
+struct OrzContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
